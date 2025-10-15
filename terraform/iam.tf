04c85eb2987327041102e1d9e097cb2f1eece704
Andrws24 <rouzen2001@gmail.com>
2025-10-15 16:34:38 -0500

    Actualizacion de Terraform

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,1 +1,40 @@
-// IAM role configuration
+resource "aws_iam_role" "lambda_exec" {
+  name = "flow-service-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "flow-service-lambda-policy"
+  description = "Allow Lambda to access DynamoDB and SQS"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:*",
+          "sqs:*",
+          "logs:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
