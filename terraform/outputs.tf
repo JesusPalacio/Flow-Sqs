9c460b9afacd5db56881f884535c25fb02a7fdeb
ZullyMelisa14 <zullymelisalopez@gmail.com>
2025-10-15 17:20:24 -0500

    Actualizacion de Terraform y el Event

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,11 @@
-// Outputs for the Terraform configuration
+output "lambda_name" {
+  value = aws_lambda_function.payment_flow.function_name
+}
+
+output "dynamodb_table" {
+  value = aws_dynamodb_table.payment.name
+}
+
+output "lambda_role" {
+  value = aws_iam_role.lambda_exec.arn
+}
