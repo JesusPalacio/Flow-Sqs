73e040227dbbb6661669122b57cb11df7837919b
ZullyMelisa14 <zullymelisalopez@gmail.com>
2025-10-15 17:09:45 -0500

    Terraform Updates

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,1 +1,15 @@
-// DynamoDB table configuration
+resource "aws_dynamodb_table" "payment" {
+  name         = var.payment_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "traceId"
+
+  attribute {
+    name = "traceId"
+    type = "S"
+  }
+
+  tags = {
+    Name = "payment"
+    Service = "flow-service"
+  }
+}
