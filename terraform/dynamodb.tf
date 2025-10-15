a49e9fa81f4cedc0572d2593a3a847c185ebfe98
Andrws24 <rouzen2001@gmail.com>
2025-10-15 17:03:02 -0500

    Revert "Actualizacion de Terraform"
    This reverts commit 04c85eb2987327041102e1d9e097cb2f1eece704.

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,15 +1,1 @@
-resource "aws_dynamodb_table" "payment" {
-  name         = var.payment_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "traceId"
-
-  attribute {
-    name = "traceId"
-    type = "S"
-  }
-
-  tags = {
-    Name = "payment"
-    Service = "flow-service"
-  }
-}
+// DynamoDB table configuration
